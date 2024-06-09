7c2ba6651b6d93a4bd48483f99d3ff89caf49877
ATISHEK SINGH <atishek.singh@techalchemy.com>
2024-06-10 00:47:09 +0530

    solved test case for castVote

--- test/test.js
+++ test/test.js
@@ -28,7 +28,7 @@ describe("Lock", function () {
   let governor = {
     name: "WE LOVE TALLY DAO",
     votingDelay: 0,
-    votingPeriod:  86400,
+    votingPeriod:  172800, // 12 second per block 
     quorumNumerator: 4,
     proposalThreshold: 0, // Set a non-zero value to prevent proposal spam.
     voteExtension: 7200 // 7200 is 24 hours (assuming 12 seconds per block)
@@ -46,7 +46,7 @@ describe("Lock", function () {
     
     console.log(owner.address, "line 47 ");
   }) 
-    it("should revert if non-moderator tries to lock/unlock user", async function () {
+    it("should work the full proposal lifecycle up to executed", async function () {
     //  const [owner1] = await ethers.getSigners();
       //new ethers.Wallet("4141be2614fa25bab42c8a70429c61f68858295519ca06943d54b960574ec82a");
     // owner = personalAccount[0];//await ethers.getSigners();
@@ -91,6 +91,12 @@ describe("Lock", function () {
     // approve the token 
     const amountToMint = 100000n;
     await token.mint("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9", amountToMint);
+    await token.mint(owner.address, amountToMint);
+    await token.delegate(owner.address);
+    await token.delegate("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9");
+
+    
+    await expect( token.grantRole(await token.MINTER_ROLE(),timelock_address)).to.emit(token, "RoleGranted");
 
     const balanceOne = await token.balanceOf("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9");
     console.log(balanceOne, "balanceOnce`");
@@ -179,6 +185,8 @@ describe("Lock", function () {
     // await expect( governor.connect(owner).castVoteuser("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9",proposalId, 1)).to.be.reverted;
       console.log(await sablier.aggregateOverflowVotes());
       console.log(await governor.proposalDeadline(proposalId), "proposalDeadline")
+      console.log(await governor.state(proposalId),"proposal id");
+      console.log(await governor.state(proposalId),"proposal id");
    
     //  const numberOfBlocks = Number(await governor.votingDelay()) + 100;
     //  await mineBlocks(numberOfBlocks);
@@ -187,8 +195,48 @@ describe("Lock", function () {
      await network.provider.send("evm_increaseTime", [86400]) // Increase time by 2 Days => 86400 * 2 => 172800
      await network.provider.send("evm_mine")
      console.log(await time.latest(), "time after ");
+
     //  Vote
      await expect( governor.castVoteuser("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9",proposalId, 1n)).to.emit(governor, "VoteCast");
+     await expect( governor.castVoteuser("0x4b15Fa59ba3e46F20e3D43CF30a9693944E1B1D9",proposalId, 1n)).to.emit(governor, "VoteCast");
+
+    // // check votes count 
+
+    //     // Queue proposal
+    //     await  expect( governor.queue(proposalId)).to.be.reverted;
+
+    //     // Wait for voting period to end
+    //     await network.provider.send("evm_increaseTime", [86400]) // Increase time by 2 Days => 86400 * 2 => 172800
+    //     await network.provider.send("evm_mine")
+
+    //     // expect proposal state to be succeeded
+    //     let proposalState = await governor.state(proposalId);
+    //     expect(proposalState).to.be.equal(4);
+
+    //     // Queue proposal
+    //     await expect( governor.queue(proposalId)).to.emit(governor, "ProposalQueued");
+
+    //     // expect proposal state to be queued
+    //     proposalState = await governor.state(proposalId);
+    //     expect(proposalState).to.be.equal(5);
+
+    //     // Execute proposal
+    //     await expect( governor.execute(proposalId)).to.be.reverted;
+
+    //     // Simulate time delay required before execution
+    //     // Replace 'executionDelay' with your contract's specific delay
+    //     await mine( 86400 +1);
+
+    //     // Execute proposal
+    //     await expect( governor.execute(proposalId)).to.emit(governor, "ProposalExecuted");
+
+    //     // expect proposal state to be executed
+    //     proposalState = await governor.state(proposalId);
+    //     expect(proposalState).to.be.equal(7);
+
+    //     // Check if admin's balance has increased
+    //     const balance = await token.balanceOf(signers.admin.address);
+    //     expect(balance).to.be.equal(11000n);
 
   });
 });
